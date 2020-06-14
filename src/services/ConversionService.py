d7bb741b2359ed5eb261c6cb90ffd46f3f864375
Shane <shane@shanedowling.com>
2020-06-14 10:59:18 +0100

    merge: combining config changes

--- src/services/ConversionService.py
+++ src/services/ConversionService.py
@@ -10,15 +10,14 @@ class AbstractConversionService(ABC):
 
 
 class FfmpgConversionService(AbstractConversionService):
-    def __init__(self, FILE_PATH, output_type="mp4"):
+    def __init__(self, FILE_PATH):
         self.file_path = FILE_PATH
-        self.output_type = output_type
 
-    def convert(self, video_name):
+    def convert(self, video_name, output_type="mp4"):
         command = "ffmpeg -i files/%s.ts -c:v libx264 -c:a aac files/%s.%s" % (
             video_name,
             video_name,
-            self.output_type,
+            output_type,
         )
         p = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, shell=False)
         p.communicate()
